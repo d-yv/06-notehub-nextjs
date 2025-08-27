a00b7d5bf9b606a41132309b45651f8b0d5ff1a3
d-yv <yuri.dubrovin@gmail.com>
2025-08-27 19:04:44 +0300

    refactoring note list

---
+++ app/notes/page.tsx
@@ -0,0 +1,18 @@
+import css from "./NotePage.module.css";
+import NoteListClient from "./Notes.client";
+import { QueryClient } from "@tanstack/react-query";
+import { fetchNotes } from "@/lib/api";
+
+export default async function App() {
+  const queryClient = new QueryClient();
+
+  queryClient.prefetchQuery({
+    queryKey: ["notes", { query: "", page: 1 }],
+    queryFn: () => fetchNotes(1, ""),
+  });
+  return (
+    <div className={css.app}>
+      <NoteListClient />
+    </div>
+  );
+}
